b0ada34733514b9fa049ececd8fcb89b3786c70c
James Graham <james.graham@dal.ca>
2020-02-11 14:47:15 -0400

    https://github.com/jamesjoegraham/engi2203-team13-project.git Main atmel studio project and configurations uploaded. this is a test of git.

---
+++ engi2203project/main.c
@@ -0,0 +1,18 @@
+/*
+ * engi2203project.c
+ *
+ * Created: 2020-02-11 2:46:10 PM
+ * Author : james
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
