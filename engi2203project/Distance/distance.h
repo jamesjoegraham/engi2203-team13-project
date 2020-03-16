b69ef9d0eb8ae017e8501dc8a0f9879a7230018b
Sahil2k20 <49459538+Sahil2k20@users.noreply.github.com>
2020-03-16 17:15:59 -0300

    Add files via upload

---
+++ engi2203project/Distance/distance.h
@@ -0,0 +1,19 @@
+/*
+ * distance.h
+ *
+ * Created: 2020-03-16 5:03:21 PM
+ *  Author: sahil
+ */ 
+
+
+#ifndef DISTANCE_H_
+#define DISTANCE_H_
+
+
+void Trigger(void);
+
+int distance(void);
+
+
+
+#endif /* DISTANCE_H_ */
